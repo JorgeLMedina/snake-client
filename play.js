19e4b97c6094acb30ec80f6258d9cd252b168991
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-18 11:29:56 -0700

    Host direction adjusted and conn.on function adjusted to take data instead of a connection

--- play.js
+++ play.js
@@ -3,11 +3,11 @@ const net = require("net");
 // Establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "172.22.47.143",
+    host: "localhost",
     port: 50541
   });
 
-  conn.on("connect", (data) => {
+  conn.on("data", (data) => {
     console.log("Message from server:", data);
   });
 
