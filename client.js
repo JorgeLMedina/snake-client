880ba3bd68d9b5beadd7df355c1fccae39c40655
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-18 11:38:14 -0700

    Includes and exports connect function that was in play.js

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+// Establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.on("data", (data) => {
+    console.log("Message from server:", data);
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
