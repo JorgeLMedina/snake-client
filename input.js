5e3ae83c09bba1ef64975e550b4336e4aeecc253
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-19 13:48:28 -0700

    Added connection variable to contain the connection object from client.js through play.js, added commands to be sent to server for movement instructions

--- input.js
+++ input.js
@@ -1,17 +1,29 @@
-// finishes game when ctrl + c is hit
-const handleUserInput = function (data) {
-  if (data === "\u0003") {
-    process.exit();
-  }
-};
+let connection;
+
 // Receives input from clients's keyboard
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
   return stdin;
+};
+
+// finishes game when ctrl + c is hit
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    process.exit();
+  } else if (data === "w") {
+    connection.write("Move: up");
+  } else if (data === "a") {
+    connection.write("Move: left");
+  } else if (data === "s") {
+    connection.write("Move: down");
+  } else if (data === "d") {
+    connection.write("Move: right");
+  }
 };
 
 module.exports = setupInput;
