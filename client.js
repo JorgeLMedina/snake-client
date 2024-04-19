1e281b51fd942233c51f5ac0324cccaf6700c83b
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-19 10:42:52 -0700

    Added a move command(commented out for future reference and experimentation

--- client.js
+++ client.js
@@ -7,6 +7,21 @@ const connect = function () {
     port: 50541
   });
 
+  const handleUserInput = function (data) {
+    if (data === "\u0003") {
+      process.exit();
+    }
+  };
+
+  const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput(data));
+    return stdin;
+  };
+
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: JMM");
